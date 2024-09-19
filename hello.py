f52f51860e3c334378184782293b29031bf7d730
avanegas <arco527@gmail.com>
2024-09-19 18:01:25 -0500

    Add files and folders css, js and img

--- hello.py
+++ hello.py
@@ -1,31 +1,58 @@
-from flask import Flask
-# from markupsafe import escape
+from flask import Flask, render_template, url_for
+from markupsafe import escape
+from datetime import datetime
 
 #en la instancia app se le dice que hello.py es el archivo principal
 app = Flask(__name__)
 
+#Filtro
+@app.add_template_filter
+def today(date):
+    return date.strftime('%d-%m-%Y')
+
+# app.add_template_filter(today, 'today')
+
+# Función personalizada
+
+@app.add_template_global
+def repeat(s, n):
+    return s * n
+
+# app.add_template_global(repeat, 'repeat')
 #decorador
 @app.route('/')
-@app.route('/index')
 def index():
-    return "<h1>Página de Inicio</h1>"
+    print(url_for('index'))
+    print(url_for('hello', name = "Angie"))
+    print(url_for('code', code = 'print("Hola")'))
+    name = "Angie"
+    friends = ["Caro", "Oliva", "Diana"]
+    date = datetime.now()
+    return render_template('index.html',
+                            name=name, 
+                            friends=friends, 
+                            date=date,
+                            # repeat=repeat 
+                            )
 
 @app.route('/hello')
 @app.route('/hello/<string:name>')
 @app.route('/hello/<string:name>/<int:age>')
-def hello(name = None, age=None):
-    if name == None and age == None:
-        return f'<h1>Hola desde Index!</h1>'
-    elif age == None:
-        return f'<h1>Hola {name}!</h1>'
-    else:
-        return f'<h1>Hola {name}, sé que tienes {age} años!</h1>'
+@app.route('/hello/<string:name>/<int:age>/<email>')
+def hello(name = None, age=None, email=None):
+    my_data = {
+        'name':name,
+        'age':age,
+        'email':email
+    }
+
+    return render_template('hello.html',data=my_data)
     
 # #Para prevenir de ataques maliciosos
 
-# @app.route('/code/<path:code>')
-# def code(code):
-#     return f'<code>{escape(code)}</code>'
+@app.route('/code/<path:code>')
+def code(code):
+    return f'<code>{escape(code)}</code>'
     
 
 if __name__ == "__main__":
